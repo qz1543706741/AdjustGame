b1c4b65d5e7ec901669828fd801a615ffcdb6635
qinzhen3 <qinzhen3@example.com>
2020-12-13 21:07:27 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    motto: 'Hi 开发者！',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+
+  getUserInfo: function (e) {
+    wx.navigateTo({
+      url: '../chooseLevel/chooseLevel',
+    })
+
+  }
+})
