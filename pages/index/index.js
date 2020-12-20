7730e0aba783ca70a13f42a52e66f7a40cfeaec1
qinzhen3 <qinzhen3@example.com>
2020-12-20 21:58:18 +0800

    feat:优化异步逻辑

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,6 +2,10 @@
 // 获取应用实例
 
 const app = getApp();
+const {
+  getLoginCode,
+  getLoginInfo
+} = require('../../utils/util.js')
 
 Page({
   data: {
@@ -14,38 +18,47 @@ Page({
 
   },
 
+  insertUserInfo: function (data) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: app.globalData.serverUrl + '/setUserInfo',
+        data,
+        success: (res) => {
+          resolve(res)
+        },
+        fail: (res) => {
+          reject(res)
+        }
+      })
+    })
+  },
+
   getUserInfo: function (e) {
     if (e.detail.userInfo) {
-      wx.login({
-        success: (res) => {
-          // 发送 res.code 到后台换取 openId, sessionKey, unionId
-          wx.request({
-            url: app.globalData.logUrl,
-            data: {
-              appid: wx.getAccountInfoSync().miniProgram.appId,
-              secret: '307d806d2f8954766c33d9be00ad429f',
-              grant_type: 'authorization_code',
-              js_code: res.code
-            },
-            success: (res) => {
-              wx.request({
-                url: app.globalData.serverUrl + '/setUserInfo',
-                data: {
-                  ...e.detail.userInfo,
-                  ...res.data
-                },
-                success: (res) => {
-                  console.log(res);
-                }
-              })
-            }
+      const userLoginInfo = async () => {
+        try {
+          const code = await getLoginCode()
+          const loginInfo = await getLoginInfo(code)
+          return await this.insertUserInfo({
+            ...(e.detail.userInfo),
+            ...loginInfo.data
           })
+        } catch (err) {
+          console.error(err);
         }
-      });
-      wx.navigateTo({
-        url: '../chooseLevel/chooseLevel'
-      });
+      }
+
+      userLoginInfo().then((r)=>{
+        console.log(r);
+        wx.navigateTo({
+          url: '../chooseLevel/chooseLevel'
+        });
+      })
+
     }
 
-  }
+  },
+
+
+
 });
--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,5 @@
+const app = getApp();
+
 const formatTime = (date) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
@@ -13,7 +15,40 @@ const formatNumber = (n) => {
   n = n.toString();
   return n[1] ? n : '0' + n;
 };
+const getLoginCode = () => {
+  return new Promise((resolve, reject) => {
+    wx.login({
+      success: (res) => {
+        resolve(res.code)
+      },
+      fail: (res) => {
+        reject(res.errMsg)
+      }
+    })
+  })
+}
+const getLoginInfo = (code) => {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: app.globalData.logUrl,
+      data: {
+        appid: wx.getAccountInfoSync().miniProgram.appId,
+        secret: '307d806d2f8954766c33d9be00ad429f',
+        grant_type: 'authorization_code',
+        js_code: code
+      },
+      success: (res) => {
+        resolve(res)
+      },
+      fail: (res) => {
+        reject(res)
+      }
+    })
+  })
+}
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  getLoginCode: getLoginCode,
+  getLoginInfo: getLoginInfo
 };
